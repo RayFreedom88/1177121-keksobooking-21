68f245e385bd65072d230b6c5a7d33c931473475
RayFreedom88 <i.musin88@mail.ru>
2020-09-24 00:11:41 +0200

    3.16 Личный проект: часть 1

---
+++ js/main.js
@@ -0,0 +1,124 @@
+'use strict';
+
+const USERS_COUNT = 8;
+const AVATARS = [
+  `img/avatars/user01.png`,
+  `img/avatars/user02.png`,
+  `img/avatars/user03.png`,
+  `img/avatars/user04.png`,
+  `img/avatars/user05.png`,
+  `img/avatars/user06.png`,
+  `img/avatars/user07.png`,
+  `img/avatars/user08.png`
+];
+const TITLE = [
+  `Первый заголовок`,
+  `Второй заголовок`,
+  `Третий заголовок`,
+  `Четвертый заголовок`,
+  `Пятый заголовок`,
+  `Шестой заголовок`,
+  `Седьмой заголовк`,
+  `Восьмой заголовок`
+];
+const TYPES = [`palace`, `flat`, `house`, `bungalo`];
+const TIMES = [`12:00`, `13:00`, `14:00`];
+const FEATURES = [`wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`];
+const DESCRIPTIONS = [
+  `Первое описание`,
+  `Второе описание`,
+  `Третье описание`,
+  `Четвертое описание`,
+  `Пятое описание`,
+  `Шестое описание`,
+  `Седьмое описание`,
+  `Восьмое описание`
+];
+const PHOTOS = [
+  `http://o0.github.io/assets/images/tokyo/hotel1.jpg`,
+  `http://o0.github.io/assets/images/tokyo/hotel2.jpg`,
+  `http://o0.github.io/assets/images/tokyo/hotel3.jpg`
+];
+const MIN_LOCATION_X = 0;
+const MAX_LOCATION_X = document.querySelector(`.map`).offsetWidth;
+const MIN_LOCATION_Y = 130;
+const MAX_LOCATION_Y = 630;
+const MIN_WIDTH_PINS = 50;
+const MIN_HEIGHT_PINS = 70;
+
+
+let mapElement = document.querySelector(`.map`);
+let mapPinsElement = mapElement.querySelector(`.map__pins`);
+
+let getActivePage = function () {
+  mapElement.classList.remove(`map--faded`);
+};
+getActivePage();
+
+let getRandomValue = function (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+let getRandomElement = function (items) {
+  return items[Math.round(Math.random() * (items.length - 1))];
+};
+
+let getBookingsMock = function () {
+  let mocks = [];
+  for (let i = 1; i <= USERS_COUNT; i++) {
+    mocks.push({
+      author: {
+        avatar: getRandomElement(AVATARS),
+      },
+      offer: {
+        title: getRandomElement(TITLE),
+        address: getRandomValue(MIN_LOCATION_X, MAX_LOCATION_X) + `, ` + getRandomValue(MIN_LOCATION_Y, MAX_LOCATION_Y),
+        price: getRandomValue(1, 1000),
+        type: getRandomElement(TYPES),
+        rooms: getRandomValue(1, 3),
+        gusts: getRandomValue(1, 10),
+        checkin: getRandomElement(TIMES),
+        checkout: getRandomElement(TIMES),
+        features: getRandomElement(FEATURES),
+        description: getRandomElement(DESCRIPTIONS),
+        photos: getRandomElement(PHOTOS),
+      },
+
+      location: {
+        x: getRandomValue(MIN_LOCATION_X, MAX_LOCATION_X),
+        y: getRandomValue(MIN_LOCATION_Y, MAX_LOCATION_Y),
+      },
+    });
+  }
+
+  return mocks;
+};
+
+let getPin = function (object) {
+  let pinTemplate = document.querySelector(`#pin`).content;
+  let pinElement = pinTemplate.querySelector(`.map__pin`).cloneNode(true);
+
+  pinElement.querySelector(`img`).src = object.author.avatar;
+  pinElement.querySelector(`img`).alt = object.offer.description;
+
+  let locationX = object.location.x - MIN_WIDTH_PINS + `px`;
+  let locationY = object.location.y - MIN_HEIGHT_PINS + `px`;
+
+  pinElement.style.left = locationX;
+  pinElement.style.top = locationY;
+
+  mapPinsElement.appendChild(pinElement);
+
+  return pinElement;
+};
+
+let createPins = function (objects) {
+  let fragment = document.createDocumentFragment();
+
+  objects.forEach(function (object) {
+    fragment.appendChild(getPin(object));
+  });
+  mapPinsElement.appendChild(fragment);
+};
+
+createPins(getBookingsMock());
