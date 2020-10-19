375d8a034c8cd824aab3ac752d354d80e31c9385
RayFreedom88 <i.musin88@mail.ru>
2020-10-19 22:55:29 +0200

    20. Личный проект: часть 2

---
+++ js/debounce.js
@@ -0,0 +1,18 @@
+'use strict';
+(function () {
+  let DEBOUNCE_INTERVAL = 500; // ms
+
+  window.debounce = function (cb) {
+    let lastTimeout = null;
+
+    return function () {
+      let parameters = arguments;
+      if (lastTimeout) {
+        window.clearTimeout(lastTimeout);
+      }
+      lastTimeout = window.setTimeout(function () {
+        cb.apply(null, parameters);
+      }, DEBOUNCE_INTERVAL);
+    };
+  };
+})();
