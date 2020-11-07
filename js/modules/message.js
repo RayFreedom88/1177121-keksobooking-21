e320ee9b05c0c1be5198773e9b073cfc544bd42b
RayFreedom88 <i.musin88@mail.ru>
2020-11-07 15:21:12 +0200

    защита v.2.2.13

--- js/modules/message.js
+++ js/modules/message.js
@@ -5,59 +5,93 @@ let mainElement = document.querySelector(`main`);
 let errorTemplate = document.querySelector(`#error`).content.querySelector(`.error`);
 let successTemplate = document.querySelector(`#success`).content.querySelector(`.success`);
 
-let onErrorSend = function (message) {
-  let errorElement = errorTemplate.cloneNode(true);
-  errorElement.querySelector(`.error__message`).textContent = message;
-  errorElement.querySelector(`.error__button`).addEventListener(`click`, onErrorMessageClick);
-  mainElement.appendChild(errorElement);
+let onResultSend = function (message) {
+  if (message) {
+    let errorMessageClose = function () {
+      let errorElement = document.querySelector(`.error`);
+      errorElement.remove();
+      document.removeEventListener(`keydown`, onErrorMessageEscKeydown);
+      document.removeEventListener(`mousedown`, onErrorMessageMouseDown);
+    };
 
-  document.addEventListener(`keydown`, onErrorMessageEscKeydown);
-  document.addEventListener(`mousedown`, onErrorMessageMouseDown);
+    let onErrorMessageClick = function () {
+      errorMessageClose();
+    };
+
+    let onErrorMessageMouseDown = function () {
+      errorMessageClose();
+    };
+
+    let onErrorMessageEscKeydown = function (evt) {
+      window.util.isEscEvent(evt, errorMessageClose);
+    };
+
+    let cloneErrorElement = errorTemplate.cloneNode(true);
+    cloneErrorElement.querySelector(`.error__message`).textContent = message;
+    cloneErrorElement.querySelector(`.error__button`).addEventListener(`click`, onErrorMessageClick);
+    mainElement.appendChild(cloneErrorElement);
+
+    document.addEventListener(`keydown`, onErrorMessageEscKeydown);
+    document.addEventListener(`mousedown`, onErrorMessageMouseDown);
+  } else {
+    let successMessageClose = function () {
+      let successElement = document.querySelector(`.success`);
+      successElement.remove();
+      document.removeEventListener(`keydown`, onSuccessMessageEscKeydown);
+      document.removeEventListener(`mousedown`, onSuccessMessageMouseDown);
+    };
+
+    let onSuccessMessageMouseDown = function () {
+      successMessageClose();
+    };
+
+    let onSuccessMessageEscKeydown = function (evt) {
+      window.util.isEscEvent(evt, successMessageClose);
+    };
+
+    let cloneSuccessElement = successTemplate.cloneNode(true);
+    mainElement.appendChild(cloneSuccessElement);
+
+    document.addEventListener(`keydown`, onSuccessMessageEscKeydown);
+    document.addEventListener(`mousedown`, onSuccessMessageMouseDown);
+  }
 };
 
-let errorMessageClose = function () {
-  let errorElement = document.querySelector(`.error`);
-  errorElement.remove();
-  document.removeEventListener(`keydown`, onErrorMessageEscKeydown);
-  document.removeEventListener(`mousedown`, onErrorMessageMouseDown);
-};
+// let errorMessageClose = function () {
+//   let errorElement = document.querySelector(`.error`);
+//   errorElement.remove();
+//   document.removeEventListener(`keydown`, onErrorMessageEscKeydown);
+//   document.removeEventListener(`mousedown`, onErrorMessageMouseDown);
+// };
 
-let onErrorMessageClick = function () {
-  errorMessageClose();
-};
+// let onErrorMessageClick = function () {
+//   errorMessageClose();
+// };
 
-let onErrorMessageMouseDown = function () {
-  errorMessageClose();
-};
+// let onErrorMessageMouseDown = function () {
+//   errorMessageClose();
+// };
 
-let onErrorMessageEscKeydown = function (evt) {
-  window.util.isEscEvent(evt, errorMessageClose);
-};
+// let onErrorMessageEscKeydown = function (evt) {
+//   window.util.isEscEvent(evt, errorMessageClose);
+// };
 
-let onSuccessSend = function () {
-  let successElement = successTemplate.cloneNode(true);
-  mainElement.appendChild(successElement);
+// let successMessageClose = function () {
+//   let successElement = document.querySelector(`.success`);
+//   successElement.remove();
+//   document.removeEventListener(`keydown`, onSuccessMessageEscKeydown);
+//   document.removeEventListener(`mousedown`, onSuccessMessageMouseDown);
+// };
 
-  document.addEventListener(`keydown`, onSuccessMessageEscKeydown);
-  document.addEventListener(`mousedown`, onSuccessMessageMouseDown);
-};
+// let onSuccessMessageMouseDown = function () {
+//   successMessageClose();
+// };
 
-let successMessageClose = function () {
-  let successElement = document.querySelector(`.success`);
-  successElement.remove();
-  document.removeEventListener(`keydown`, onSuccessMessageEscKeydown);
-  document.removeEventListener(`mousedown`, onSuccessMessageMouseDown);
-};
-
-let onSuccessMessageMouseDown = function () {
-  successMessageClose();
-};
-
-let onSuccessMessageEscKeydown = function (evt) {
-  window.util.isEscEvent(evt, successMessageClose);
-};
+// let onSuccessMessageEscKeydown = function (evt) {
+//   window.util.isEscEvent(evt, successMessageClose);
+// };
 
 window.message = {
-  onErrorSend,
-  onSuccessSend
+  onErrorSend: onResultSend,
+  onSuccessSend: onResultSend
 };
